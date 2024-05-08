d8333095e361c7ef390272dbbe203a8e36388221
Jo-Jae <whwodjs11@naver.com>
2024-05-08 17:59:42 +0900

    components2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import Logo from "./Logo";
 import NavBar from "./NavBar";
+import ButtonMore from "./ButtonMore";
 
 const Header = () => {
   return (
     <header className="bg-blue-100 px-2 py-4 flex justify-between">
       <Logo />
       <NavBar />
-      {/* ButtonMore */}
-      <button className="button-style inline-block md:hidden">...</button>
+      <ButtonMore />
     </header>
   );
 };

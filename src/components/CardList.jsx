77fef7a68159de4ec10134311e59ad861f1b3b12
Jo-Jae <whwodjs11@naver.com>
2024-05-08 16:04:41 +0900

    components

---
+++ src/components/CardList.jsx
@@ -0,0 +1,18 @@
+import Card from "./CardList";
+
+const CardList = () => {
+  return (
+    <ul className="bg-red-100 mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 justify-items-center gap-4">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </ul>
+  );
+};
+export default CardList;
